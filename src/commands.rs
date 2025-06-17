2f2e33bce9c7172da62100aac8dfa6fbeac78d2e
Ege Dalcan <exd251@case.edu>
2025-06-17 17:11:02 -0400

    added the new files

---
+++ src/commands.rs
@@ -0,0 +1,14 @@
+use std::net::TcpStream;
+
+pub fn req_diag(stream: &TcpStream) {
+    
+}
+
+pub fn check_alive(stream: &TcpStream) {
+    todo!()
+}
+
+pub fn req_data(stream: &TcpStream) {
+    todo!()
+}
+
---
+++ src/station.rs
@@ -0,0 +1,11 @@
+use systemstat::{System, Platform};
+use lazy_static::lazy_static;
+
+struct Station {
+    system: System,
+    last_check: usize,
+}
+
+lazy_static! {
+
+}
